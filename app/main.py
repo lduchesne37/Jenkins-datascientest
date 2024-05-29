1d61009e6edcee0c8d4f4cc85ad59470a411bbfc
lduchesne37 <132375774+lduchesne37@users.noreply.github.com>
2024-05-29 14:10:54 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline !! And plus that, it doesn't works !!"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline !! And plus that, it doesn't works !! Maybe now !!"}
